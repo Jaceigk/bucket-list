600a8fe30b5bdbcedeb2fc256021c76a57e34afd
JamesGood626 <jamesgood626@gmail.com>
2019-03-05 14:53:44 -0500

    Logging out the newest, oldest, and number of items on add.

--- main.js
+++ main.js
@@ -1,73 +1,71 @@
 // Global variables ftw
 
-
 let isStack = true;
+let stack = [];
 
 // Set init to run when the window loads.
 window.onload = init;
 
 function init() {
+  // Set event handlers.
+  document.querySelector("#submit").addEventListener("click", addNewItem);
 
-    // Set event handlers.
-    document.querySelector('#submit')
-        .addEventListener('click', addNewItem)
+  document.querySelector("#remove").addEventListener("click", removeItem);
 
-    document.querySelector('#remove')
-        .addEventListener('click', removeItem);
-
-    document.querySelector('#toggle')
-            .addEventListener('click', toggleQueueAndStack);
+  document
+    .querySelector("#toggle")
+    .addEventListener("click", toggleQueueAndStack);
 }
 
 function addNewItem(event) {
-    // Prevent page reload.
-    event.preventDefault()
-    
-    // Get the value from the input field.
-    const newItem = document.querySelector('#new-item').value;
+  // Prevent page reload.
+  event.preventDefault();
 
-    // Set the input field back to blank.
-    resetInput();
+  // Get the value from the input field.
+  const newItem = document.querySelector("#new-item").value;
 
-    // Add the item to the <ul>.
-    displayItem(newItem);
+  // Set the input field back to blank.
+  resetInput();
 
-    // Now comes your part: add the item to the list.
+  // Add the item to the <ul>.
+  displayItem(newItem);
 
+  console.log("The newest item: ", newItem);
+  stack.unshift(newItem);
+  console.log("The oldest item: ", stack[stack.length - 1]);
+  console.log("The number of items: ", stack.length);
+  // Now comes your part: add the item to the list.
 
-    // Display it in next-item if it's the first item:
-    if(false) { // definitely change that condition!
-        document.querySelector('#next-item').innerText = ''; // Replace that empty string with the actual item!
-    } 
+  // Display it in next-item if it's the first item:
+  if (false) {
+    // definitely change that condition!
+    document.querySelector("#next-item").innerText = ""; // Replace that empty string with the actual item!
+  }
 
-    document.querySelector('#newest-item').innerText = '' // Replace that empty string with the actual item!
+  document.querySelector("#newest-item").innerText = ""; // Replace that empty string with the actual item!
 
-    document.querySelector('#number-of-items').innerText = 0 // Replace that with the number of items!
+  document.querySelector("#number-of-items").innerText = 0; // Replace that with the number of items!
 }
 
 function removeItem(event) {
-    // Prevent page reload.
-    event.preventDefault()
+  // Prevent page reload.
+  event.preventDefault();
 
-    if(isStack) {
-        removeLastFromPage();
-        // Your code to remove it from the array  goes here!
-
-
-    } else {
-        removeFirstFromPage();
-        // Your code to remove it from the array goes here!
-
-
-    }
+  if (isStack) {
+    removeLastFromPage();
+    // Your code to remove it from the array  goes here!
+  } else {
+    removeFirstFromPage();
+    // Your code to remove it from the array goes here!
+  }
 }
 
 function toggleQueueAndStack(event) {
-    // Prevent page reload.
-    event.preventDefault()
+  // Prevent page reload.
+  event.preventDefault();
 
-    // How can we toggle whether it's a stack or a queue?
-    // Your code below!
+  // How can we toggle whether it's a stack or a queue?
+  // Your code below!
 }
 
 /*
@@ -78,25 +76,25 @@ Feel free to check it out though.
 */
 
 function removeLastFromPage() {
-    const items = document.querySelectorAll('li');
-    const lastItem = items[items.length - 1];
-    lastItem.parentNode.removeChild(lastItem);
+  const items = document.querySelectorAll("li");
+  const lastItem = items[items.length - 1];
+  lastItem.parentNode.removeChild(lastItem);
 }
 
 function removeFirstFromPage() {
-    const items = document.querySelectorAll('li');
-    const firstItem = items[0];
-    firstItem.parentNode.removeChild(firstItem);
+  const items = document.querySelectorAll("li");
+  const firstItem = items[0];
+  firstItem.parentNode.removeChild(firstItem);
 }
 
 function resetInput() {
-    // Resets input field to blank. No need to add anything here!
-    document.querySelector('#new-item').value = '';
+  // Resets input field to blank. No need to add anything here!
+  document.querySelector("#new-item").value = "";
 }
 
 function displayItem(itemText) {
-    // Displays item on list. No need to add anything here!
-    const newItem = document.createElement('li');
-    newItem.innerText = itemText;
-    document.querySelector('#items').appendChild(newItem);
+  // Displays item on list. No need to add anything here!
+  const newItem = document.createElement("li");
+  newItem.innerText = itemText;
+  document.querySelector("#items").appendChild(newItem);
 }
